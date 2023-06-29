ff80169352aaf5fa21c3d3b9e26d6338746a0273
Rymma Savchuk <savchukrmm@gmail.com>
2023-06-29 19:10:41 +0300

    added displaying wrong request

--- src/components/Image.tsx
+++ src/components/Image.tsx
@@ -1,10 +1,18 @@
+import { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { useMediaQuery } from 'react-responsive';
 
 import { RootState } from '../redux/store';
 
 const Image: React.FC = () => {
+  const [wrongInput, setWrongInput] = useState(false);
+
   const { words, status } = useSelector((state: RootState) => state.words);
+  const { word } = useSelector((state: RootState) => state.search);
+
+  useEffect(() => {
+    status === 'error' && setWrongInput(word);
+  }, [status]);
 
   const isDesktop = useMediaQuery({ maxWidth: 650 });
 
@@ -41,7 +49,10 @@ const Image: React.FC = () => {
       {((status === 'error' && !words.word) ||
         (status === 'success' && !words.results)) && (
         <div className="sorry">
-          <h3>You have no matching search terms.</h3>
+          <h3>
+            You have no matching search terms of{' '}
+            <span className="wrong-request">'{wrongInput}'</span>
+          </h3>
           <h4>Please make sure that you've entered the word correctly.</h4>
           <img
             width={300}
